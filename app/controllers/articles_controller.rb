b675f3b84b9c3e0a419ef058a10135a9386c99c7
Reef Daddy <campbellsharif@gmail.com>
2018-05-07 11:22:44 -0400

    home page mostly styled

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,13 +12,16 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:user_id])
-      @article = @user.articles.create(article_params)
-      redirect_to article_path(@article)
+    #@article = current_user
+    #@user = User.find(params[:user_id])
+    #@article = @user.articles.create(article_params)
+    @article = Article.find(params[:id])
+      #redirect_to article_path(@article)
 end
 
   def create
     @article = Article.new(article_params)
+    #ask if i need this line to solve my problem
     @article.user_id = session[:user_id]
     if @article.save
     redirect_to @article
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
 
   root 'welcome#index'
+  get  '/contact',  to: 'welcome#contact'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
@@ -11,7 +12,9 @@ Rails.application.routes.draw do
 
   resources :users
 
-  resources :articles
+  resources :articles do
+  resources :comments
+end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
