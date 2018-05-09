632bf34941aa56cb0b260edddf19354c9359d3ec
Reef Daddy <campbellsharif@gmail.com>
2018-05-09 00:23:59 -0400

    more styling done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,127 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+var race = {
+  //dashboard.moves makes the pop-up visible
+altmer : function() {
+  document.getElementById("altmer").style.visibility = "visible";
+
+  document.getElementById("argonian").style.visibility = "hidden";
+  document.getElementById("bosmer").style.visibility = "hidden";
+  document.getElementById("breton").style.visibility = "hidden";
+  document.getElementById("dunmer").style.visibility = "hidden";
+  document.getElementById("imperial").style.visibility = "hidden";
+  document.getElementById("khajiit").style.visibility = "hidden";
+  document.getElementById("nord").style.visibility = "hidden";
+  document.getElementById("orsimer").style.visibility = "hidden";
+  document.getElementById("redguard").style.visibility = "hidden";
+}, argonian : function() {
+  document.getElementById("argonian").style.visibility = "visible";
+
+  document.getElementById("altmer").style.visibility = "hidden";
+  document.getElementById("bosmer").style.visibility = "hidden";
+  document.getElementById("breton").style.visibility = "hidden";
+  document.getElementById("dunmer").style.visibility = "hidden";
+  document.getElementById("imperial").style.visibility = "hidden";
+  document.getElementById("khajiit").style.visibility = "hidden";
+  document.getElementById("nord").style.visibility = "hidden";
+  document.getElementById("orsimer").style.visibility = "hidden";
+  document.getElementById("redguard").style.visibility = "hidden";
+}, bosmer : function() {
+  document.getElementById("bosmer").style.visibility = "visible";
+
+  document.getElementById("altmer").style.visibility = "hidden";
+  document.getElementById("argonian").style.visibility = "hidden";
+  document.getElementById("breton").style.visibility = "hidden";
+  document.getElementById("dunmer").style.visibility = "hidden";
+  document.getElementById("imperial").style.visibility = "hidden";
+  document.getElementById("khajiit").style.visibility = "hidden";
+  document.getElementById("nord").style.visibility = "hidden";
+  document.getElementById("orsimer").style.visibility = "hidden";
+  document.getElementById("redguard").style.visibility = "hidden";
+}, breton : function() {
+  document.getElementById("breton").style.visibility = "visible";
+
+  document.getElementById("altmer").style.visibility = "hidden";
+  document.getElementById("bosmer").style.visibility = "hidden";
+  document.getElementById("argonian").style.visibility = "hidden";
+  document.getElementById("dunmer").style.visibility = "hidden";
+  document.getElementById("imperial").style.visibility = "hidden";
+  document.getElementById("khajiit").style.visibility = "hidden";
+  document.getElementById("nord").style.visibility = "hidden";
+  document.getElementById("orsimer").style.visibility = "hidden";
+  document.getElementById("redguard").style.visibility = "hidden";
+}, dunmer : function() {
+  document.getElementById("dunmer").style.visibility = "visible";
+
+  document.getElementById("altmer").style.visibility = "hidden";
+  document.getElementById("bosmer").style.visibility = "hidden";
+  document.getElementById("argonian").style.visibility = "hidden";
+  document.getElementById("breton").style.visibility = "hidden";
+  document.getElementById("imperial").style.visibility = "hidden";
+  document.getElementById("khajiit").style.visibility = "hidden";
+  document.getElementById("nord").style.visibility = "hidden";
+  document.getElementById("orsimer").style.visibility = "hidden";
+  document.getElementById("redguard").style.visibility = "hidden";
+}, imperial : function() {
+  document.getElementById("imperial").style.visibility = "visible";
+
+  document.getElementById("altmer").style.visibility = "hidden";
+  document.getElementById("bosmer").style.visibility = "hidden";
+  document.getElementById("argonian").style.visibility = "hidden";
+  document.getElementById("breton").style.visibility = "hidden";
+  document.getElementById("dunmer").style.visibility = "hidden";
+  document.getElementById("khajiit").style.visibility = "hidden";
+  document.getElementById("nord").style.visibility = "hidden";
+  document.getElementById("orsimer").style.visibility = "hidden";
+  document.getElementById("redguard").style.visibility = "hidden";
+}, khajiit : function() {
+  document.getElementById("khajiit").style.visibility = "visible";
+
+  document.getElementById("altmer").style.visibility = "hidden";
+  document.getElementById("bosmer").style.visibility = "hidden";
+  document.getElementById("argonian").style.visibility = "hidden";
+  document.getElementById("breton").style.visibility = "hidden";
+  document.getElementById("dunmer").style.visibility = "hidden";
+  document.getElementById("imperial").style.visibility = "hidden";
+  document.getElementById("nord").style.visibility = "hidden";
+  document.getElementById("orsimer").style.visibility = "hidden";
+  document.getElementById("redguard").style.visibility = "hidden";
+}, nord : function() {
+  document.getElementById("nord").style.visibility = "visible";
+
+  document.getElementById("altmer").style.visibility = "hidden";
+  document.getElementById("bosmer").style.visibility = "hidden";
+  document.getElementById("argonian").style.visibility = "hidden";
+  document.getElementById("breton").style.visibility = "hidden";
+  document.getElementById("dunmer").style.visibility = "hidden";
+  document.getElementById("imperial").style.visibility = "hidden";
+  document.getElementById("khajiit").style.visibility = "hidden";
+  document.getElementById("orsimer").style.visibility = "hidden";
+  document.getElementById("redguard").style.visibility = "hidden";
+}, orsimer : function() {
+  document.getElementById("orsimer").style.visibility = "visible";
+
+  document.getElementById("altmer").style.visibility = "hidden";
+  document.getElementById("bosmer").style.visibility = "hidden";
+  document.getElementById("argonian").style.visibility = "hidden";
+  document.getElementById("breton").style.visibility = "hidden";
+  document.getElementById("dunmer").style.visibility = "hidden";
+  document.getElementById("imperial").style.visibility = "hidden";
+  document.getElementById("khajiit").style.visibility = "hidden";
+  document.getElementById("nord").style.visibility = "hidden";
+  document.getElementById("redguard").style.visibility = "hidden";
+}, redguard : function() {
+  document.getElementById("redguard").style.visibility = "visible";
+
+  document.getElementById("altmer").style.visibility = "hidden";
+  document.getElementById("bosmer").style.visibility = "hidden";
+  document.getElementById("argonian").style.visibility = "hidden";
+  document.getElementById("breton").style.visibility = "hidden";
+  document.getElementById("dunmer").style.visibility = "hidden";
+  document.getElementById("imperial").style.visibility = "hidden";
+  document.getElementById("khajiit").style.visibility = "hidden";
+  document.getElementById("nord").style.visibility = "hidden";
+  document.getElementById("orsimer").style.visibility = "hidden";
+}
+};
